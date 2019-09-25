adf686c6537cff834090efefdd968b0d39dd35bc
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-09-25 09:13:26 +0900

    Add

---
+++ 結婚/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/mattn/go-colorable"
+
+func main() {
+	out := colorable.NewColorableStdout()
+	fmt.Fprintf(out, "ʕ◔ϖ◔ʔ"+"\x1b[31m"+"💓"+"\x1b[0m"+"ʕ◔ϖ◔ʔ")
+}
